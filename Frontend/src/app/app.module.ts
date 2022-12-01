b360445322b75890552e86af75b120014da9b1a5
T154N <santiiariias2001@gmail.com>
2022-12-01 19:01:36 -0300

    acerca de

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogosAPComponent } from './components/logos-ap/logos-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogosAPComponent,
     RedesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
